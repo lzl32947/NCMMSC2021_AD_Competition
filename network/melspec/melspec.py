4984b85cce1e8a75c55c24d3557577364c6644f8
lzl32947 <lzl32947@outlook.com>
2021-07-11 20:29:27 +0800

    Update the Mel-Spec model to be more simple and easy to train.

--- network/melspec/melspec.py
+++ network/melspec/melspec.py
@@ -2,8 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as func
 
-from network.melspec.extractor.conv_batchnorm import ExtractionModel
-from network.melspec.stand_alone.denses_conv_batchnorm import DenseModel
+from network.melspec.extractor.standard_conv import ExtractionModel
 
 
 class MelSpecModel(nn.Module):
@@ -11,9 +10,7 @@ class MelSpecModel(nn.Module):
         super().__init__()
 
         self.extractor = ExtractionModel()
-        self.dense = DenseModel(6656)
 
     def forward(self, input_tensor: torch.Tensor):
         output = self.extractor(input_tensor)
-        output = self.dense(output)
         return output
