379b397d0d49f40146f17e7825a127771cfae55d
lzl32947 <lzl32947@outlook.com>
2021-07-15 11:53:28 +0800

    Update the comments in trainer_util.py

--- util/train_util/trainer_util.py
+++ util/train_util/trainer_util.py
@@ -1,11 +1,17 @@
-from typing import List, Dict
+from typing import List, Dict, Generic
 from torch.utils.data.dataloader import DataLoader
 from configs.types import AudioFeatures, DatasetMode
 from util.train_util.data_loader import AldsDataset
 
 
-def prepare_feature(feature_list: List[str]):
+def prepare_feature(feature_list: List[str]) -> List[AudioFeatures]:
+    """
+    This function is to get which features are used in dataset.
+    :param feature_list: List[str], and should be the configs['features']
+    :return: list[AudioFeatures]
+    """
     use_features = []
+    # Traverse the feature list to convert them into AudioFeatures
     for item in AudioFeatures:
         if item.value in feature_list:
             use_features.append(item)
@@ -13,6 +19,17 @@ def prepare_feature(feature_list: List[str]):
 
 
 def prepare_dataloader(use_features: List[AudioFeatures], configs: Dict, run_for: DatasetMode, **kwargs):
+    """
+    This function returns the generator of dataloader.
+    Considering the k-fold is used in the program so the function is design to be the generator.
+    Notice that even the k-fold is not used the function will still return a dataloader generator with the only one dataloader.
+    :param use_features: List[AudioFeatures], the list of AudioFeatures and determines which features are used in dataset.
+    :param configs: Dict, and should be configs['dataset'] by default
+    :param run_for: DatasetMode, this parameters in used to determine what the dataset aims to.
+    :param kwargs: other parameters, notice that any given parameters will override the parameters in config
+    :return: generators of dataloader, the length is same as the k_fold
+    """
+    # override the parameters in configs if given in kwargs
     use_merge = configs['use_merge'] if 'use_merge' not in kwargs.keys() else kwargs['use_merge']
     repeat_times = configs['repeat_times'] if 'repeat_times' not in kwargs.keys() else kwargs['repeat_times']
     k_fold = configs['k_fold'] if 'k_fold' not in kwargs.keys() else kwargs['k_fold']
@@ -20,6 +37,7 @@ def prepare_dataloader(use_features: List[AudioFeatures], configs: Dict, run_for
     random_disruption = configs['random_disruption'] if 'random_disruption' not in kwargs.keys() else kwargs[
         'random_disruption']
     if k_fold != 0:
+        # Generate the k_fold dataloader
         for fold in range(k_fold):
             dataset = AldsDataset(use_features=use_features, use_merge=use_merge,
                                   repeat_times=repeat_times, configs=configs['process'], k_fold=k_fold,
@@ -29,6 +47,7 @@ def prepare_dataloader(use_features: List[AudioFeatures], configs: Dict, run_for
             dataloader = DataLoader(dataset, batch_size=batch_size)
             yield dataloader
     else:
+        # Generate the single dataloader
         for fold in range(1):
             dataset = AldsDataset(use_features=use_features, use_merge=use_merge,
                                   repeat_times=repeat_times, configs=configs['process'],
