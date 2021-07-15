6095aef37abed95706bb02a61d5c74064985be70
lzl32947 <lzl32947@outlook.com>
2021-07-15 11:38:00 +0800

    Update the comments in the logger.py

--- util/log_util/logger.py
+++ util/log_util/logger.py
@@ -6,9 +6,14 @@ import time
 
 
 class GlobalLogger(object):
+    """
+    This is the class of the global logger and is set to be the single-instance when running
+    """
+    # Add lock in the instance for threading in case
     _instance_lock = threading.Lock()
 
-    def __init__(self):
+    def __init__(self) -> None:
+        # Check the attr in __init__ for not initializing the parameters in re-creating the instance
         if not hasattr(self, "log"):
             self.log = None
         if not hasattr(self, "config"):
@@ -17,35 +22,50 @@ class GlobalLogger(object):
             self.log_name = None
 
     def __new__(cls, *args, **kwargs):
+        # This function is used to lock the instance for only create the single instance when running
         if not hasattr(GlobalLogger, "_instance"):
             with GlobalLogger._instance_lock:
                 if not hasattr(GlobalLogger, "_instance"):
                     GlobalLogger._instance = object.__new__(cls)
         return GlobalLogger._instance
 
-    def config_logger(self):
-        self.log.setLevel(logging.INFO)
-
     def get_logger(self) -> logging.Logger:
+        """
+        Get the global logger.
+        :return: logging.Logger instance, and in class return the instance of GlobalLogger
+        """
         return self.log
 
     def get_config(self) -> Dict:
+        """
+        Return the configuration of logger.
+        :return: Dict, config files of the logger
+        """
         return self.config
 
-    def init_config(self, config, store_name):
+    def init_config(self, config: Dict, store_name: str) -> None:
+        """
+        Init the logger with the given parameters.
+        :param config: Dict, the configs
+        :param store_name: str, the name of the saving name
+        :return: None
+        """
+        # Set logger name
         self.log = logging.getLogger("main")
+        # Set logger level
         self.log.setLevel(logging.DEBUG)
         self.config = config
         self.log_name = store_name
-
+        # Init the console logger
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
         formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
         ch.setFormatter(formatter)
         self.log.addHandler(ch)
-
+        # Create the log directory with the store_name if not exist(and should be not exist)
         if not os.path.exists(os.path.join(self.config["log_dir"], self.log_name)):
             os.mkdir(os.path.join(self.config["log_dir"], self.log_name))
+        # Init the file logger
         fh = logging.FileHandler(os.path.join(self.config['log_dir'], self.log_name, "run.log"), mode="w")
         fh.setLevel(logging.INFO)
         formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
