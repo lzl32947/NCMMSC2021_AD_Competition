84124482bfad3e91e8b9c620974a293943e8c534
yyy <2432947210@qq.com>
2021-07-16 17:41:27 +0800

    Fill the lstm_model.

--- network/general_model.py
+++ network/general_model.py
@@ -7,7 +7,7 @@ from torch.autograd import Variable
 class DenseModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.linear_1 = nn.Linear(6240, 64)
+        self.linear_1 = nn.Linear(9000, 64)
         self.dropout = nn.Dropout(0.3)
         self.linear_2 = nn.Linear(64, 3)
 
@@ -17,7 +17,7 @@ class DenseModel(nn.Module):
     def forward(self, input_tensor: torch.Tensor):
         batch_size = input_tensor.shape[0]
 
-        output = input_tensor.view((batch_size, -1))
+        output = input_tensor.reshape((batch_size, -1))
 
         output = self.linear_1(output)
         output = func.relu(output, inplace=True)
@@ -88,21 +88,17 @@ class ExtractionModel(nn.Module):
 class lstm_model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv_layer_1 = nn.Conv2d(32, 32, (3, 1), (2, 1))
-        self.conv_layer_2 = nn.Conv2d(32, 32, (3, 1), (2, 1))
+        self.conv_layer_1 = nn.Conv2d(96, 96, (3, 1), (2, 1))
+        self.conv_layer_2 = nn.Conv2d(96, 96, (3, 1), (2, 1))
 
-        self.conv_layer_3 = nn.Conv2d(32, 32, (3, 1), (2, 1))
+        self.conv_layer_3 = nn.Conv2d(96, 96, (2, 1), (2, 1))
 
-        self.conv_layer_4 = nn.Conv2d(32, 32, (3, 1), (2, 1))
-        self.conv_layer_5 = nn.Conv2d(32, 32, (2, 1), (2, 1))
-        self.bilstm_layer_1 = nn.LSTM(input_size=32, hidden_size=300, num_layers=2, bidirectional=True,
+        self.bilstm_layer_1 = nn.LSTM(input_size=96, hidden_size=300, num_layers=2, bidirectional=True,
                                       batch_first=True)
 
         self._normal_init(self.conv_layer_1, 0, 0.1)
         self._normal_init(self.conv_layer_2, 0, 0.1)
         self._normal_init(self.conv_layer_3, 0, 0.1)
-        self._normal_init(self.conv_layer_4, 0, 0.1)
-        self._normal_init(self.conv_layer_5, 0, 0.1)
         for name, param in self.bilstm_layer_1.named_parameters():
             if name.startswith("weight"):
                 nn.init.xavier_normal_(param)
@@ -114,8 +110,6 @@ class lstm_model(nn.Module):
         output = self.conv_layer_1(input_tensor)
         output = self.conv_layer_2(output)
         output = self.conv_layer_3(output)
-        output = self.conv_layer_4(output)
-        output = self.conv_layer_5(output)
         print(output.size())
 
 
@@ -124,7 +118,6 @@ class lstm_model(nn.Module):
         output = output.permute((0, 3, 1, 2))
 
         output = output.view(batch_size, length, channel)
-        print(output.size())
         hidden = Variable(torch.zeros(2 * 2, batch_size, 300))
         hidden = hidden.cuda()
         cell = Variable(torch.zeros(2 * 2, batch_size, 300))
@@ -198,16 +191,19 @@ class GeneralModel(nn.Module):
         self.extractor_mfcc = ExtractionModel()
         self.extractor_spec = ExtractionModel()
         self.extractor_mel = ExtractionModel()
-        self.dense = ConcatModel()
+        self.lstm = lstm_model()
+        self.dense = DenseModel()
 
     def forward(self, input_mfcc: torch.Tensor, input_spec: torch.Tensor, input_mel: torch.Tensor):
         output_mfcc = self.extractor_mfcc(input_mfcc)
         output_spec = self.extractor_spec(input_spec)
         output_mel = self.extractor_mel(input_mel)
         concat_output = torch.cat([output_spec, output_mel, output_mfcc], dim=1)
-        output = self.dense(concat_output)
+        output_lstm = self.lstm(concat_output)
+        output = self.dense(output_lstm)
         return output
 
 if __name__ == '__main__':
     torchinfo.summary(ExtractionModel(), (4, 1, 513, 157))
     torchinfo.summary(lstm_model(), (4, 32, 61, 15))
+    torchinfo.summary(DenseModel(), (4, 15, 96))
