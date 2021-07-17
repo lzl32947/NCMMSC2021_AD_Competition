50d5ae8129fcff0e66b16459d1aec2f68b857093
yyy <2432947210@qq.com>
2021-07-17 22:26:34 +0800

    Upload the merge_vad data.

--- bin/trainer_specific.py
+++ bin/trainer_specific.py
@@ -6,6 +6,7 @@ from util.tools.files_util import global_init
 from util.train_util.trainer_util import prepare_feature, prepare_dataloader, read_weight, get_best_acc_weight, \
     train_general, train_specific_feature
 
+
 if __name__ == '__main__':
     """
     This is a template for joint-features training
