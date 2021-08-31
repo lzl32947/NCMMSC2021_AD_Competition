cf212bd4a63874cc683eb51d1e9081771ea4633a
lzl32947@outlook.com <lzl32947@outlook.com>
2021-08-31 15:24:27 +0800

    refactor: Remove the unnecessary codes
    1. Remove all codes that excluded from the project.

--- util/wav_process/time.py
+++
@@ -1,8 +0,0 @@
-import wave
-import contextlib
-fname = 'data/sa1.wav'
-with contextlib.closing(wave.open(fname,'r')) as f:
-    frames = f.getnframes()
-    rate = f.getframerate()
-    duration = frames / float(rate)
-    print(duration)
