6cf5f1ccd1db3a7e5d5ab7542ba5622c5b4c1422
yyyssy <yangyanyu@stu.xjtu.edu.cn>
2021-09-15 23:09:57 +0800

    readme

---
+++ model/models/model_competition_long.py
@@ -0,0 +1,260 @@
+from typing import Tuple
+
+import torchvision.models
+from torch import nn
+import torch
+import torch.nn.functional as func
+from torch.utils import model_zoo
+
+from model.base_model import BaseModel
+from model.manager import Register, Registers
+from model.modules.vgg import VggNetBackbone
+
+class DenseModel(nn.Module):
+    def __init__(self, input_unit=6240):
+        super().__init__()
+        self.linear_1 = nn.Linear(input_unit, 64)
+        self.dropout = nn.Dropout(0.3)
+        self.linear_2 = nn.Linear(64, 3)
+
+    def forward(self, input_tensor: torch.Tensor):
+        batch_size = input_tensor.shape[0]
+
+        output = input_tensor.view((batch_size, -1))
+
+        output = self.linear_1(output)
+        output = func.relu(output)
+        output = self.dropout(output)
+
+        output = self.linear_2(output)
+        return output
+
+
+class ExtractionModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv_layer_1 = nn.Conv2d(1, 32, (3, 3))
+        self.maxpooling_1 = nn.MaxPool2d((2, 4), stride=2)
+        self.batchnorm_1 = nn.BatchNorm2d(32)
+
+        self.conv_layer_2 = nn.Conv2d(32, 32, (3, 3))
+        self.maxpooling_2 = nn.MaxPool2d((4, 4), stride=2)
+        self.batchnorm_2 = nn.BatchNorm2d(32)
+
+        self.conv_layer_3 = nn.Conv2d(32, 32, (3, 3))
+        self.maxpooling_3 = nn.MaxPool2d((2, 5), stride=2)
+        self.batchnorm_3 = nn.BatchNorm2d(32)
+
+    def forward(self, input_tensor: torch.Tensor):
+        batch_size = input_tensor.shape[0]
+
+        output = self.conv_layer_1(input_tensor)
+        output = func.relu(output)
+        output = self.maxpooling_1(output)
+        output = self.batchnorm_1(output)
+
+        output = self.conv_layer_2(output)
+        output = func.relu(output)
+        output = self.maxpooling_2(output)
+        output = self.batchnorm_2(output)
+
+        output = self.conv_layer_3(output)
+        output = func.relu(output)
+        output = self.maxpooling_3(output)
+        output = self.batchnorm_3(output)
+
+        return output
+
+
+class ConcatModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv_layer_1 = nn.Conv2d(96, 256, (3, 3))
+
+        self.conv_layer_2 = nn.Conv2d(256, 512, (3, 3))
+        self.maxpooling_2 = nn.MaxPool2d((2, 2), stride=2)
+
+        self.conv_layer_3 = nn.Conv2d(512, 1024, (3, 3))
+        self.maxpooling_3 = nn.MaxPool2d((2, 2), stride=2)
+
+        self.linear_1 = nn.Linear(1024, 1024)
+        self.dropout_1 = nn.Dropout(0.3)
+        self.linear_2 = nn.Linear(1024, 3)
+
+    def forward(self, input_tensor: torch.Tensor):
+        batch_size = input_tensor.shape[0]
+
+        output = self.conv_layer_1(input_tensor)
+        output = func.relu(output)
+
+        output = self.conv_layer_2(output)
+        output = func.relu(output)
+        output = self.maxpooling_2(output)
+
+        output = self.conv_layer_3(output)
+        output = func.relu(output)
+        output = self.maxpooling_3(output)
+
+        output = output.view((batch_size, -1))
+        output = self.linear_1(output)
+        output = self.dropout_1(output)
+        output = self.linear_2(output)
+        return output
+
+
+@Registers.model.register
+class CompetitionSpecificTrainLongModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+        self.extractor = ExtractionModel()
+        self.dense = DenseModel()
+        self.pool = nn.AdaptiveAvgPool2d((13, 15))
+
+    def forward(self, input_tensor: torch.Tensor):
+        output = self.extractor(input_tensor)
+        output = self.pool(output)
+        output = self.dense(output)
+        return output
+
+
+@Registers.model.register
+class CompetitionMSMJointConcatFineTuneLongModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+        self.extractor_mfcc = ExtractionModel()
+        self.extractor_spec = ExtractionModel()
+        self.extractor_mel = ExtractionModel()
+        self.pool1 = nn.AdaptiveAvgPool2d((13, 15))
+        self.pool2 = nn.AdaptiveAvgPool2d((13, 15))
+        self.pool3 = nn.AdaptiveAvgPool2d((13, 15))
+        self.dense = ConcatModel()
+
+    def forward(self, input_mfcc: torch.Tensor, input_spec: torch.Tensor, input_mel: torch.Tensor):
+        output_mfcc = self.extractor_mfcc(input_mfcc)
+        output_spec = self.extractor_spec(input_spec)
+        output_mel = self.extractor_mel(input_mel)
+        output_mfcc = self.pool1(output_mfcc)
+        output_spec = self.pool2(output_spec)
+        output_mel = self.pool3(output_mel)
+        concat_output = torch.cat([output_spec, output_mel, output_mfcc], dim=1)
+        output = self.dense(concat_output)
+        return output
+
+
+@Registers.model.register
+class CompetitionMSMJointConcatFineTuneResNet18BackboneLongModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+        self.extractor_mfcc = Registers.module["ResNetBackbone"](18)
+        self.extractor_spec = Registers.module["ResNetBackbone"](18)
+        self.extractor_mel = Registers.module["ResNetBackbone"](18)
+        self.dense = ResNet18ConcatModel()
+
+    def forward(self, input_mfcc: torch.Tensor, input_spec: torch.Tensor, input_mel: torch.Tensor):
+        output_mfcc = self.extractor_mfcc(input_mfcc)
+        output_spec = self.extractor_spec(input_spec)
+        output_mel = self.extractor_mel(input_mel)
+        concat_output = torch.cat([output_spec, output_mel, output_mfcc], dim=1)
+        output = self.dense(concat_output)
+        return output
+
+
+class ResNet18ConcatModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv_layer_1 = nn.Conv2d(512 * 3, 512, (3, 3))
+
+        self.maxpooling_2 = nn.MaxPool2d((2, 2))
+
+        self.linear_1 = nn.Linear(512 * 11, 512)
+        self.dropout_1 = nn.Dropout(0.3)
+        self.linear_2 = nn.Linear(512, 3)
+
+    def forward(self, input_tensor: torch.Tensor):
+        batch_size = input_tensor.shape[0]
+
+        output = self.conv_layer_1(input_tensor)
+        output = func.relu(output)
+
+        output = self.maxpooling_2(output)
+
+        output = output.view((batch_size, -1))
+        output = self.linear_1(output)
+        output = self.dropout_1(output)
+        output = self.linear_2(output)
+        return output
+
+
+@Registers.model.register
+class SpecificTrainVggNet19BackboneLongModel(BaseModel):
+    def __init__(self, input_shape: Tuple):
+        super(SpecificTrainVggNet19BackboneLongModel, self).__init__()
+        self.extractor = Registers.module["VggNetBackbone"](19)
+        self.fc = nn.Linear(512 * 7 * 7, 512)
+        self.fc2 = nn.Linear(512, 64)
+        self.fc3 = nn.Linear(64, 3)
+
+    def forward(self, input_tensor: torch.Tensor):
+        batch_size = input_tensor.shape[0]
+        output = self.extractor(input_tensor)
+        long_out = output.view(batch_size, -1)
+        long_out = self.fc(long_out)
+        long_out2 = func.relu(long_out)
+        long_out2 = self.fc2(long_out2)
+        long_out3 = func.relu(long_out2)
+        long_out3 = self.fc3(long_out3)
+        return long_out3
+
+
+@Registers.model.register
+class CompetitionSpecificTrainVggNet19BNBackboneLongModel(BaseModel):
+    def __init__(self):
+        super(CompetitionSpecificTrainVggNet19BNBackboneLongModel, self).__init__()
+        # self.extractor = Registers.module["VggNetBackbone"]("19_bn")
+        self.extractor = VggNetBackbone("19_bn")
+        self.conv1 = nn.Conv2d(512, 1024, (3, 3))
+        self.max_pooling = nn.MaxPool2d((2, 2), stride=2)
+        self.pooling = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, 1024)
+        self.dropout1 = nn.Dropout(0.3)
+        self.fc2 = nn.Linear(1024, 256)
+        self.dropout2 = nn.Dropout(0.3)
+        self.fc3 = nn.Linear(256, 64)
+        self.dropout3 = nn.Dropout(0.3)
+        self.fc4 = nn.Linear(64, 3)
+
+    def forward(self, input_tensor: torch.Tensor):
+        batch_size = input_tensor.shape[0]
+        output = self.extractor(input_tensor)
+        output = self.conv1(output)
+        output = func.relu(output)
+        output = self.max_pooling(output)
+        output = self.pooling(output)
+        long_out = output.view(batch_size, -1)
+        long_out = self.fc(long_out)
+        long_out2 = func.relu(long_out)
+        long_out2 = self.dropout1(long_out2)
+        long_out2 = self.fc2(long_out2)
+        long_out3 = func.relu(long_out2)
+        long_out3 = self.dropout2(long_out3)
+        long_out3 = self.fc3(long_out3)
+        long_out4 = func.relu(long_out3)
+        long_out4 = self.dropout3(long_out4)
+        long_out4 = self.fc4(long_out4)
+        return long_out4
+
+
+
+
+if __name__ == "__main__":
+    import torchinfo
+
+    model = CompetitionSpecificTrainVggNet19BNBackboneLongModel()
+    model.cuda()
+    torchinfo.summary(model, (4, 3, 128, 782))
+    # model = SpecificTrainResNet34BackboneLongModel(input_shape=())
+    # # model.cuda()
+    # torchinfo.summary(model, (4, 1, 128, 782))
---
+++ model/modules/vgg.py
@@ -0,0 +1,64 @@
+import math
+import torch
+import torch.nn as nn
+import torchvision
+from torch.utils import model_zoo
+
+from model.manager import Registers
+import torchvision.models
+
+model_urls = {
+    'vgg11': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
+    'vgg13': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
+    'vgg16': 'https://download.pytorch.org/models/vgg16-397923af.pth',
+    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
+    'vgg11_bn': 'https://download.pytorch.org/models/vgg11_bn-6002323d.pth',
+    'vgg13_bn': 'https://download.pytorch.org/models/vgg13_bn-abd245e5.pth',
+    'vgg16_bn': 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth',
+    'vgg19_bn': 'https://download.pytorch.org/models/vgg19_bn-c79401a0.pth',
+}
+
+
+@Registers.module.register
+class VggNetBackbone(nn.Module):
+    def __init__(self, num_layers: str = "19"):
+        super().__init__()
+        if num_layers not in ["11", "13", "16", "19", "19_bn"]:
+            raise RuntimeError("Layers should be in 11, 13, 16, 19, 19_bn")
+        model = None
+        if num_layers == "11":
+            model = torchvision.models.vgg11()
+            state_dict = model_zoo.load_url('https://download.pytorch.org/models/vgg11-bbd30ac9.pth')
+            model.load_state_dict(state_dict, strict=False)
+        elif num_layers == "13":
+            model = torchvision.models.vgg13()
+            state_dict = model_zoo.load_url('https://download.pytorch.org/models/vgg13-c768596a.pth')
+            model.load_state_dict(state_dict, strict=False)
+        elif num_layers == "16":
+            model = torchvision.models.vgg16()
+            state_dict = model_zoo.load_url('https://download.pytorch.org/models/vgg16-397923af.pth')
+            model.load_state_dict(state_dict, strict=False)
+        elif num_layers == "19":
+            model = torchvision.models.vgg19()
+            state_dict = model_zoo.load_url('https://download.pytorch.org/models/vgg19-dcbb9e9d.pt')
+            model.load_state_dict(state_dict, strict=False)
+        else:
+            model = torchvision.models.vgg19_bn()
+            state_dict = model_zoo.load_url('https://download.pytorch.org/models/vgg19_bn-c79401a0.pth')
+            model.load_state_dict(state_dict, strict=False)
+
+        backbone = list(
+            [model.features]
+        )
+        self.model = nn.Sequential(*backbone)
+
+    def forward(self, input_tensor: torch.Tensor):
+        return self.model(input_tensor)
+
+
+if __name__ == '__main__':
+    import torchinfo
+
+    model = torchvision.models.vgg19()
+    model = model.cuda()
+    torchinfo.summary(model, (2, 3, 128, 157))
