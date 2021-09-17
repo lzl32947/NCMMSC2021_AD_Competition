d3fc94c5c3ef35ec86bc30816ddd8103f2e68fa6
lzl32947 <lzl32947@outlook.com>
2021-09-17 12:17:12 +0800

    doc(Readme): Update the evaluator.py
    1. Update the evaluator.py, warning: NOT TESTED!

--- bin/evaluator.py
+++ bin/evaluator.py
@@ -20,6 +20,175 @@ from util.train_util.trainer_util import prepare_dataloader, get_best_acc_weight
 torch.multiprocessing.set_sharing_strategy('file_system')
 
 
+def evaluate_multi_joint(identifier: str, config: Dict, model_list: List[Dict], **kwargs):
+    # Get the fold
+    total_fold = config['dataset']['k_fold']
+    logger = GlobalLogger().get_logger()
+
+    output_all_predict = dict()
+    output_all_correct = dict()
+    output_all_data = dict()
+    output_all_source = dict()
+    dict_output = {}
+
+    for index, model_unit in enumerate(model_list):
+        model_name = model_unit["model_name"]
+        use_features = model_unit["use_features"]
+        weight_identifier = model_unit["weight_identifier"]
+        input_channels = model_unit["input_channels"]
+        joint = model_unit["joint"]
+        dataset_func = model_unit["dataset_func"]
+        weight_description = model_unit["weight_description"]
+
+        logger.info("Evaluate the model {}".format(model_name))
+
+        output_feature_predict = []
+        output_feature_correct = []
+        output_feature_source = []
+        output_feature_data = []
+
+        total = 0
+        correct = 0
+
+        logger.info("Evaluate the model {}.".format(model_name))
+        for current_fold, test_dataloader in enumerate(
+                prepare_dataloader([use_features], config["dataset"], DatasetMode.TEST,
+                                   # repeat_times=5 * config["dataset"]["repeat_times"]
+                                   repeat_times=1
+                    , dataset_func=dataset_func)):
+
+            output_fold_predict = []
+            output_fold_correct = []
+            output_fold_source = []
+            output_fold_data = []
+
+            # Get model and send to GPU
+            model = Registers.model[model_name]()
+            model = model.cuda()
+            # Load weight file into model
+            weight_file = get_best_acc_weight(
+                os.path.join(config['weight']['weight_dir'], weight_identifier),
+                total_fold, current_fold,
+                use_features.value if isinstance(use_features, AudioFeatures) else weight_description)
+            logger.info("Using weight {}".format(weight_file))
+            model.load_state_dict(torch.load(weight_file), strict=True)
+            # Set to eval mode
+            model.eval()
+
+            # Get the length of the test dataloader
+            length = len(test_dataloader)
+
+            # Init the bar
+            bar_test = tqdm(range(length))
+            bar_test.set_description(
+                "Testing using feature {}, for fold {}/{}.".format(use_features, current_fold,
+                                                                   total_fold))
+
+            # Re-init the timer
+            current_time = time.time()
+
+            with torch.no_grad():
+                # Running one batch
+                for data in test_dataloader:
+                    # Get the features
+                    feature_list = []
+                    label = data[AudioFeatures.LABEL]
+                    file_name = data[AudioFeatures.NAME]
+                    output_fold_correct.append(label.numpy())
+                    output_fold_source.append(file_name)
+                    label = label.cuda()
+                    if not joint:
+                        features = data[use_features]
+                        if input_channels != 1:
+                            features = torch.cat([features] * input_channels, dim=1)
+
+                        features = features.cuda()
+                        output = model(features)
+                    else:
+                        for item in use_features:
+                            feature = data[item]
+                            if input_channels != 1:
+                                feature = torch.cat([feature] * input_channels, dim=1)
+                            feature = feature.cuda()
+                            feature_list.append(feature)
+                        output = model(*feature_list)
+
+                    # Normalize the output to one-hot mode
+                    predictions = func.softmax(output, dim=1)
+                    _, predicted = torch.max(predictions, 1)
+
+                    output_fold_data.append(predictions.cpu().numpy())
+                    output_fold_predict.append(predicted.cpu().numpy())
+
+                    # Record the size
+                    total += label.size(0)
+                    # Record the correct output
+                    correct += (predicted == label).sum().item()
+                    # Calculate the accuracy
+                    acc = correct / total
+                    # Visualize the accuracy
+                    bar_test.set_postfix(acc=acc)
+                    # Update the bar
+                    bar_test.update(1)
+
+                output_feature_correct.append(output_fold_correct)
+                output_feature_predict.append(output_fold_predict)
+                output_feature_source.append(output_fold_source)
+                output_feature_data.append(output_fold_data)
+            bar_test.close()
+            # Time the timer
+            now_time = time.time()
+            logger.info(
+                "Finish testing feature {}, for fold {}/{}, time cost {:.2f}s ,with acc {:.2f}%".format(
+                    use_features.value if isinstance(use_features, AudioFeatures) else weight_description,
+                    current_fold,
+                    total_fold,
+                    now_time - current_time,
+                    acc * 100))
+        output_all_correct[index] = output_feature_correct
+        output_all_predict[index] = output_feature_predict
+        output_all_data[index] = output_feature_data
+        output_all_source[index] = output_feature_source
+    logger.info("All results evaluation finished!")
+
+    output_list = []
+    label_list = []
+    feature_list = []
+    for index, model_unit in enumerate(model_list):
+        model_name = model_unit["model_name"]
+        use_features = model_unit["use_features"]
+        weight_description = model_unit["weight_description"]
+        output_dict, label_dict = analysis_output(identifier, config, output_all_correct[index],
+                                                  output_all_predict[index],
+                                                  output_all_data[index], output_all_source[index],
+                                                  "{} with {}".format(model_name,
+                                                                      use_features.value if isinstance(use_features,
+                                                                                                       AudioFeatures) else weight_description))
+        output_list.append(output_dict)
+        label_list.append(label_dict)
+        feature_list.append(feature)
+    files = output_list[0].keys()
+    n2s_dict = {0: "AD", 1: "HC", 2: "MCI"}
+    s2n_dict = {"AD": 1, "MCI": 2, "HC": 3}
+    with open(os.path.join(config["output"]["output_dir"], identifier, "evaluation_on_{}.txt".format("Multi")),
+              "w", encoding="utf-8") as fout:
+        for file in files:
+            fout.write(
+                "{} {} ".format(file.split("/")[-1] if "/" in file else file.split("\\")[-1],
+                                label_list[0][file]))
+            output_data_list = []
+            for index, f in enumerate(feature_list):
+                output_data = output_list[index][file]
+                pred_on_this_f = n2s_dict[int(np.argmax(output_data, axis=0))]
+                fout.write("{} {} {} ".format(output_data.tolist(), pred_on_this_f,
+                                              pred_on_this_f == label_list[index][file]))
+                output_data_list.append(output_data)
+            output_average = np.average(np.array(output_data_list), axis=0)
+            output_label = n2s_dict[int(np.argmax(output_average))]
+            fout.write("-> {} {} {}\n".format(output_average.tolist(), output_label,
+                                              output_label == label_list[0][file]))
+
+
 def evaluate_specific_joint(identifier: str, config: Dict, model_name: str, use_feature_and_weight: Dict,
                             dataset_func: Callable, input_channels: int = 1, **kwargs):
     # Get the fold
@@ -482,10 +651,34 @@ if __name__ == '__main__':
     #                          AudioFeatures.MFCC, weight_identifier, AldsDataset, input_channels=3)
     # logger.info("Analysis results for {} with {}".format(model_name, weight_identifier))
     # analysis_result(time_identifier, configs, c, p, model_name)
-    model_name = "CompetitionSpecificTrainVggNet19BNBackboneModel"
+    # model_name = "CompetitionSpecificTrainVggNet19BNBackboneModel"
     # weight_identifier = "20210905_133648"
-    evaluate_specific_joint(time_identifier, configs, model_name,
-                            {AudioFeatures.MFCC: "20210915_184216"
-                                , AudioFeatures.MELSPECS: "20210915_183611"
-                                , AudioFeatures.SPECS: "20210915_183922"},
-                            AldsDataset, input_shape=(), input_channels=3)
+    # evaluate_specific_joint(time_identifier, configs, model_name,
+    #                         {AudioFeatures.MFCC: "20210915_184216"
+    #                             , AudioFeatures.MELSPECS: "20210915_183611"
+    #                             , AudioFeatures.SPECS: "20210915_183922"},
+    #                         AldsDataset, input_shape=(), input_channels=3)
+    model_list = [
+        {"model_name": "CompetitionSpecificTrainVggNet19BNBackboneModel",
+         "use_features": AudioFeatures.MFCC,
+         "weight_identifier": "20210915_184216",
+         "input_channels": 3,
+         "joint": False,
+         "dataset_func": AldsDataset,
+         "weight_description": ""},
+        {"model_name": "CompetitionSpecificTrainVggNet19BNBackboneModel",
+         "use_features": AudioFeatures.MELSPECS,
+         "weight_identifier": "20210915_183611",
+         "input_channels": 3,
+         "joint": False,
+         "dataset_func": AldsDataset,
+         "weight_description": ""},
+        {"model_name": "MSMJointConcatFineTuneModel",
+         "use_features": [AudioFeatures.SPECS, AudioFeatures.MELSPECS, AudioFeatures.MFCC],
+         "weight_identifier": "20210904_141710",
+         "input_channels": 1,
+         "joint": True,
+         "dataset_func": AldsDataset,
+         "weight_description": "General"}
+    ]
+    evaluate_multi_joint(time_identifier, configs, model_list)
